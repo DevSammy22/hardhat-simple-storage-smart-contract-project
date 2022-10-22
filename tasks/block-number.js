2bdfd237b8021f9a1511a6fdacd522800acbb160
DevSammy22 <sadedeji1992@yahoo.com>
2022-10-22 20:52:55 +0100

    Implemented Custom Hardhat Tasks

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -3,6 +3,7 @@ const { task } = require("hardhat/config");
 task("block-number", "prints the current block number").setAction(
     // const blockTask = async function() => {}
     // async function blockTask() {}
+
     async (taskArgs, hre) => {
         const blockNumber = await hre.ethers.provider.getBlockNumber();
         console.log(`Current block number: ${blockNumber}`);
